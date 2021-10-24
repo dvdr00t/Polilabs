7a151866e4115ca835b273083ac0ca39d08fb2b7
Davide Arcolini <55852418+DavideArcolini@users.noreply.github.com>
2021-10-24 19:28:36 +0200

    Add files via upload

---
+++ exe01_writingValueIntoRegister.asm
@@ -0,0 +1,9 @@
+.MODEL small
+.STACK
+.DATA
+.CODE
+.STARTUP    
+    MOV AX, 0F12H      ; Storing in AX the hexadecimal value 0F12H, 
+                       ; which corresponds to 3858
+.EXIT
+END
