d4bf17f8b694bec6e7dbf4c9b6f8fa530556f317
Davide Arcolini <55852418+DavideArcolini@users.noreply.github.com>
2021-10-24 19:31:37 +0200

    Delete exe01_writingValueIntoRegister.asm

--- lab00/exe01_writingValueIntoRegister.asm
+++
@@ -1,9 +0,0 @@
-.MODEL small
-.STACK
-.DATA
-.CODE
-.STARTUP    
-    MOV AX, 0F12H      ; Storing in AX the hexadecimal value 0F12H, 
-                       ; which corresponds to 3858
-.EXIT
-END
