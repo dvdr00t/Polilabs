e428f4dc84e21971b7a037ccaefaa42cb6aadf67
Davide Arcolini <55852418+DavideArcolini@users.noreply.github.com>
2022-06-27 15:38:54 +0200

    removed test files

--- OpenSSL/Week01 - PRNG with OpenSSL/Exercise04/test.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-
-int main (int argc, char *argv[]) {
-    printf("%ld\n", sizeof(int));
-    exit(EXIT_SUCCESS); 
-}
